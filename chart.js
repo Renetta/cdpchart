c786d091861d98ec22e9342862b5ce9aa8f39696
Renetta <renetta92@gmail.com>
2016-10-12 09:10:35 +0530

    Update chart.js

--- chart.js
+++ chart.js
@@ -116,7 +116,8 @@
         var inputDate = null;
         inputDate = document.getElementById('inputDate').value + ' ' + document.getElementById("inputTime").value + ' ' + document.getElementById("inputMS").value +
           ' ' + document.getElementById("inputMicro").value;
-        return moment(inputDate).unix() * 1000;
+//         return moment(inputDate).unix() * 1000;
+        return moment(inputDate).format('x');
       }
       
       function plotSelectedPoint(point) {
